53a0f99d87f25b8a1fecb71c93ba6e5e12a441ac
Mario Amore <quaaludes@gmail.com>
2018-05-24 11:10:38 +0800

    Add a loop to generate enemies + strict mode

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
+"use strict";
 /******************************************************
     E N E M I E S
  *****************************************************/
 // Enemies our player must avoid
+
 class Enemy {
   constructor(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.speed = (Math.floor((Math.random() * 350) + 150));
+    this.speed = (Math.floor((Math.random() * 360) + 160));
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
   }
   // Draw the enemy on the screen, required method for game
   render() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
   update(dt) {
@@ -24,7 +27,7 @@ class Enemy {
 
     this.x = (dt * this.speed) + this.x;
     if (this.x > 500) {
-      this.speed = (Math.floor((Math.random() * 350) + 150));
+      this.speed = (Math.floor((Math.random() * 360) + 160));
       //enemy speed is changing after an enemy goes off screen
       this.x = -150;
       //enemy location resets left of canvas after an enemy goes off screen
@@ -73,6 +76,7 @@ class Player extends Enemy {
       if (this.y <= 0) {
         this.x = 200;
         this.y = 400;
+        alert("CONGRATULATIONS! YOU WON THE GAME!")
       } //nested if resets player position when it reaches water blocks(player.y = 0)
     }
     if (direction === 'right' && this.x < 400) {
@@ -90,10 +94,12 @@ class Player extends Enemy {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let enemy1 = new Enemy(-150, 60);
-let enemy2 = new Enemy(-150, 140);
-let enemy3 = new Enemy(-150, 220);
-let allEnemies = [enemy1, enemy2, enemy3];
+
+let allEnemies = [];
+for (let i = 0; i < 3; i++) {
+  let enemy = new Enemy(-150, 70 * (i + 1));
+  allEnemies.push(enemy);
+}
 
 let player = new Player(200, 400);
 
@@ -108,29 +114,3 @@ document.addEventListener('keyup', function(e) {
   };
   player.handleInput(allowedKeys[e.keyCode]);
 });
-// Reflexions of a lost developers(first day 21 of May):
-//where do I start? If I open the index.html I can't see anything, where is the canvas? How should I start
-//to see anything? Looking at the code in engine.js to get inspirations.
-//day2: 12.10pm
-//-I comment out the error in the console, and finally the canvas appear! hurray! Now it's time to position
-//the enemies
-//-setting enemy location with x, y coordinates like this: let enemy1 = new Enemy(10, 46);
-//-setting player location with another Player class based on Enemy class
-//-now every console error disappeared
-//-giving a random x-location to all enemies based on Math.floor((Math.random() * 100) + 1)
-//-giving a random speed(the same to all enemies inside update() function
-//day3: 10:52 am
-//-enemy speed and rendering it's ok, now I focus on player handleInput function:
-//after struggling for a while on how to render the movement after arrows keypress, I've read on the forum some topics that suggested me to implement a different argument in Player.prototype.handleInput function, in my case 'direction' based on the keyCode pressed. I then implement an if statement to handle the borders of the canvas to avoid the player disappear, and then reset when it reaches water blocks(y=0)
-//-I handle collisions by checking player and enemy respective position on canvas(with some margins for realistic collisions) and reset player position if the two entities have the same coords
-//Everything looks ok, I put ES6 syntax, and game it's ok
-//I need to add the following improvements, I'm behind the deadlines, need to focus on main functionalities to submit the project, I will come back to make nice improvements(the ones on TODO list)
-//
-//
-//// TODO:
-// Add Scoreboard, player lives, playerselect screen
-// Add collectible items on screen
-// Multiple vehicle types
-// Timed games
-// Be creative!
-//
